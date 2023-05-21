9e492571a720000175fede65d36653355233874e
talle <tallevinzon@gmail.com>
2023-05-22 02:12:08 +0300

    Added the ability to remove stocks

--- app/src/main/java/com/example/mstockapp/MainActivity.java
+++ app/src/main/java/com/example/mstockapp/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.mstockapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -38,7 +40,7 @@ import java.util.Map;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StockListAdapter.AdapterCallback {
 
     private static final String TAG = "MainActivity";
     EditText symbolTxtInput;
@@ -49,7 +51,25 @@ public class MainActivity extends AppCompatActivity {
     String price;
     String dailyChange;
     String jsonContent;
+    int onStartUpFlag;
 
+
+    private StockDataRetriever.StockDataListener stockDataListener = new StockDataRetriever.StockDataListener() {
+        @Override
+        public void onStockDataReceived(String symbol, double latestPrice, double dailyChange) {
+            // Handle the received stock data
+            Log.e(TAG, "Symbol: " + symbol + ", Latest Price: " + latestPrice + ", Daily Change: " + dailyChange);
+            Stock stock = new Stock(symbol,String.valueOf(latestPrice),String.valueOf(dailyChange));
+            runOnUiThread(() -> updateStockList(stock));
+
+        }
+
+        @Override
+        public void onStockDataError(String error) {
+            // Handle the error in retrieving stock data
+            Log.e(TAG, "Error: " + error);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         stockList = (ListView) findViewById(R.id.StockList);
         adapter = new StockListAdapter(this, R.layout.activity_list_view, Stocks);
         stockList.setAdapter(adapter);
+        adapter.setCallback((StockListAdapter.AdapterCallback) this);
 
 
         try {
@@ -84,91 +105,47 @@ public class MainActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Stock[] stocks = gson.fromJson(jsonContent, Stock[].class);
 
+        onStartUpFlag = 1;
         for (Stock stock : stocks) {
             // Process each stock object
+            StockDataRetriever.getStockData(stock.getSymbol(), stockDataListener);
             Stocks.add(stock);
             adapter.notifyDataSetChanged();
         }
 
 
+
         stockList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d("List_View", "Item is clicked @ position " + i);
+                Intent intent = new Intent(MainActivity.this, LineChartGraph.class);
+                intent.putExtra("key", Stocks.get(i).symbol);
+                startActivity(intent);
             }
         });
     }
 
 
+    @Override
+    public void onAdapterUpdated() {
+        // Update the adapter or perform any necessary actions
+        adapter.notifyDataSetChanged();
+    }
+
     public void fetchStockData(View view) {
-        Thread thread = new Thread(() -> {
-            OkHttpClient client = new OkHttpClient();
-            String apiKey = "JSIFUJ8C1BIJZAH4";
-            String url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbolTxtInput.getText() + "&apikey=" + apiKey;
+        onStartUpFlag = 0;
+        StockDataRetriever.getStockData(String.valueOf(symbolTxtInput.getText()), stockDataListener);
+    }
 
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-
-            try {
-                Response response = client.newCall(request).execute();
-                if (response.isSuccessful()) {
-                        ResponseBody responseBody = response.body();
-                    if (responseBody != null) {
-                        String responseData = responseBody.string();
-                        try {
-                            JSONObject jsonResponse = new JSONObject(responseData);
-                            String globalQuote = jsonResponse.getString("Global Quote");
-                            String[] pairs = globalQuote.split(",");// Create a map to store the key-value pairs
-                            Map<String, String> dataMap = new HashMap<>();
-
-                            if(!pairs[0].equals("{}")) {
-                                for (String pair : pairs) {
-                                    String[] keyValue = pair.split(":");
-                                    String key = keyValue[0].replaceAll("\"", "").trim();
-                                    String value = keyValue[1].replaceAll("\"", "").trim();
-                                    dataMap.put(key, value);
-                                }// Access the extracted key-value pairs
-                            }
-                            symbol = dataMap.get("{01. symbol");
-                            String open = dataMap.get("02. open");
-                            String high = dataMap.get("03. high");
-                            String low = dataMap.get("04. low");
-                            price = dataMap.get("05. price");
-                            String volume = dataMap.get("06. volume");
-                            String latestTradingDay = dataMap.get("07. latest trading day");
-                            String previousClose = dataMap.get("08. previous close");
-                            dailyChange = dataMap.get("09. change");
-                            String changePercent = dataMap.get("10. change percent");
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        Log.d("StockData", responseData);
-
-                        // Process the response data as per your requirements
-                        // Parse JSON data or perform other operations
-                    }
-                } else {
-                    // Handle error response
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        thread.start();
-        try {
-            thread.join();  // Wait for the thread to finish
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if(symbol != null){
-            Stocks.add(new Stock(symbol, price, dailyChange));// Print the response to the console
-            adapter.notifyDataSetChanged();
+    public void updateStockList(Stock stock){
+        if(onStartUpFlag == 0){
+            Stocks.add(stock);
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             String json = gson.toJson(Stocks);
             saveToLocalFile(json);
         }
+        adapter.notifyDataSetChanged();
+
     }
 
     public void saveToLocalFile(String json){
@@ -188,7 +165,5 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             // Error occurred while saving the file
         }
-
-
     }
 }
